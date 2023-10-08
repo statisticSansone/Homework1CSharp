2fabfd54f4ce63f46b055740bbcc4ec4c1f54b31
statisticSansone <147333358+statisticSansone@users.noreply.github.com>
2023-10-08 18:12:11 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MyHomework1
+{
+    partial class Lavagna
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 454);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Lavagna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Lavagna";
+            this.Text = "Lavagna";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Load += new System.EventHandler(this.Lavagna_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyHomework1
+{
+    public partial class Lavagna : Form
+    {
+        public Lavagna()
+        {
+            InitializeComponent();
+            panel1.Invalidate();
+        }
+
+        private void Lavagna_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gra = e.Graphics;
+            Pen pen = new Pen(Color.Yellow, 3);
+
+            //Draw a point
+            e.Graphics.DrawRectangle(pen, 250, 10, 1, 1);
+
+            //Draw a line
+            pen.Color = Color.White;
+            e.Graphics.DrawLine(pen, 200, 200, 200, 20);
+
+            //Draw a rectangle
+            pen.Color = Color.Blue;
+            Rectangle rectangle = new Rectangle(60, 200, 100, 20);
+            e.Graphics.DrawRectangle(pen, rectangle);
+
+            //Draw a circle
+            pen.Color = Color.Red;
+            Rectangle rectForEllipse = new Rectangle(30, 20, 150, 150);
+            e.Graphics.DrawEllipse(pen, rectForEllipse);
+        }
+    }
+}
